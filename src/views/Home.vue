0010a8cd7aaa8fb24ca2e40625584bd44146b211
solohad1n <fox663399@gmail.com>
2022-09-09 10:33:42 +0600

    init

---
+++ src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <h1>
+    {{ test }}
+  </h1>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      test: "Приветики",
+    };
+  },
+};
+</script>
+
+<style>
+</style>
